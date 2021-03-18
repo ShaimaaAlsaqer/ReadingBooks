6d167faf3a87dbbf81983967b057c815c7085bda
Shaimaa <shaimaeng07@gmail.com>
2021-03-18 16:48:44 +0200

    creatinhhthe html and js

---
+++ JavaScript/readBook.js
@@ -0,0 +1,20 @@
+var Shaima = alert('welcome to the page');
+var TypeOfBookYouLoveIt = prompt("what type of books");
+document.write(TypeOfBookYouLoveIt);
+
+
+var LoveReading = true;
+var DontLoveReading = false;
+if(LoveReading==true)
+{
+    alert('you are the best');
+} 
+else if(DontLoveReading= false)
+{
+    alert('you are not good and you should start reading');
+}
+else
+{
+    alert('start reading')
+}
+
